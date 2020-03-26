fe106ac865e2217551406ddc70cd58bce2a48f8f
Akribes <30956441+Akribes@users.noreply.github.com>
2020-03-26 15:04:12 +0100

    Remove nicknames if someone with that username joins

--- src/main/java/net/bnbdiscord/bnbnick/BnBNickEventListener.java
+++ src/main/java/net/bnbdiscord/bnbnick/BnBNickEventListener.java
@@ -1,10 +1,14 @@
 package net.bnbdiscord.bnbnick;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class BnBNickEventListener implements Listener {
     private BnBNickPlugin plugin;
@@ -15,9 +19,18 @@ public class BnBNickEventListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        // TODO: check if this is a new player, and if it is, check if someone else uses their name as a nickname
-
         try {
+            String uuid = plugin.getDatabase().getUUID(e.getPlayer().getName());
+            if (uuid != null) { // Name is used as a nickname
+                plugin.getDatabase().unsetNickname(uuid);
+                Player old = Bukkit.getPlayer(UUID.fromString(uuid));
+                if (old != null) {
+                    Utils.applyNickname(old, null);
+                    Utils.sendMessage(old, "BAD NEWS", "Someone with your nickname as their username joined, so your " +
+                            "nickname has been removed.", ChatColor.RED);
+                }
+            }
+
             String nickname = plugin.getDatabase().getNickname(e.getPlayer().getUniqueId().toString());
             Utils.applyNickname(e.getPlayer(), nickname);
         } catch (SQLException ex) {
